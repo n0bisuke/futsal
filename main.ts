f05ca0ac6860bc65bf95061eec1e90f53bb2d3e9
n0bisuke <rsksound@gmail.com>
2023-07-24 16:19:41 +0000

    init

--- main.ts
+++ main.ts
@@ -1,9 +1,70 @@
-const BASE_URL = `https://labola.jp/reserve/events/search/personal/area-13/`;
+
+const DOMAIN = 'labola.jp';
+const AREA = 'area-13';
+const BASE_URL = `https://${DOMAIN}/reserve/events/search/personal/${AREA}/`;
 
 const json = fetch(BASE_URL);
 
-json.then((response) => {
-  return response.json();
-}).then((jsonData) => {
-  console.log(jsonData);
-});
+//イベント情報のパース
+const parseEvent = (html:string) => {
+  const regex = /<a href=(\/r\/event\/show\/\d+\/)>([\s\S]*?)<\/a>/g;
+
+  let match;
+  const events = [];
+
+  while ((match = regex.exec(html)) !== null) {
+    const url = `https://${DOMAIN}${match[1]}`;
+    const eventHtml = match[2];
+
+    const title = /<h2>(.*?)<\/h2>/s.exec(eventHtml)?.[1];
+    const dateTimeMatch = /<div class="date">(.*?)<\/div>/s.exec(eventHtml);
+    const dateTimeRaw = dateTimeMatch ? dateTimeMatch[1].split('<span')[0] : '';  // '｜'より前の文字列を取得
+  
+    const dateRegex = /(\d{4}\/\d{2}\/\d{2})/; 
+    const date = dateRegex.exec(dateTimeRaw)?.[1];  // 日付を抽出
+  
+    const dayOfWeekRegex = /（(.*?)）/;
+    const dayOfWeek = dayOfWeekRegex.exec(dateTimeRaw)?.[1];  // 曜日を抽出
+  
+    const timeRegex = /（.*?）(\d{2}:\d{2}〜\d{2}:\d{2})/;
+    const time = timeRegex.exec(dateTimeRaw)?.[1];  // 時間を抽出
+  
+    // const place = /<div class="date">.*?｜.*?｜(.*?)<\/div>/s.exec(eventHtml)?.[1];
+    const place = /<div class="date">.*?｜.*?｜<\/span>(.*?)<\/div>/s.exec(eventHtml)?.[1];
+    let address = /<div class="address">.*?Event<\/i>(.*?)<\/div>/s.exec(eventHtml)?.[1];
+    if (address) {
+      address = address.trim();
+    }
+
+    let price = /<div class="price">.*?Event<\/i>(.*?)<\/div>/s.exec(eventHtml)?.[1];
+    if (price) {
+      price = price.replace(/<\/?span[^>]*>/g, '');
+    }
+
+    let levelText = /<div class="level">.*?<\/i><span class="ccc">｜<\/span>(.*?)<\/div>/s.exec(eventHtml)?.[1];
+    if (levelText) {
+      levelText = levelText.replace(/<\/?span[^>]*>/g, '');
+    }
+
+    const level = {
+      mix: levelText?.includes('ミックス'),
+      enjoy: levelText?.includes('エンジョイ'),
+    };
+
+    let timeRemaining = /<div class="sub">受付終了まで残り(.*?)<\/div>/s.exec(eventHtml)?.[1];
+    if (timeRemaining) {
+      timeRemaining = timeRemaining.replace(/<\/?span[^>]*>/g, '');
+    }
+  
+    events.push({ url, title, date, dayOfWeek, time, place, address, price, level, levelText, timeRemaining });
+  }
+  
+  return events;
+}
+
+json.then(response => response.text())
+    .then(html => parseEvent(html))
+    .then(async eventList => {
+      console.log(eventList);
+      await Deno.writeTextFile("output.json", JSON.stringify(eventList, null, 2));
+    });
